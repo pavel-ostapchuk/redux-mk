d0c8557d74a8395593318377f523a0e4e243766f
pvostapchuk <PVOstapchuk@sberbank.ru>
2022-10-29 11:38:16 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
-import TodosPage from 'enitites/TodosPage';
-import UsersPage from 'enitites/UsersPage';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Layout from 'shared/Layout';
-import './styles/index.scss';
 
-const App = () => (
-  <Layout>
-    <Routes>
-      <Route path="users_page" element={<UsersPage />} />
-      <Route path="todos_page" element={<TodosPage />} />
-    </Routes>
-  </Layout>
-);
+const App = () => <div>Hello!</div>;
 
 export default App;
